399d2542999d13a7fdc3c8fcc7ece743253524ba
Denis Sorokin <desoo@Deniss-MacBook-Pro.local>
2022-04-15 00:25:38 +0300

    upd and improvement

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ var dict = new Dictionary<string, string>();
 var stat = new Dictionary<string, Stat>();
 
 if (File.Exists(dictPath))
-{   
+{
     var jsonText = File.ReadAllText(dictPath);
     var jsonTextStat = File.ReadAllText(statPath);
     dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonText);
@@ -37,7 +37,7 @@ SaveDictToFile();
 
 void Menu()
 {
-   while(true)
+    while (true)
     {
         Console.WriteLine(
 @"Enter action:
@@ -77,82 +77,117 @@ void Menu()
             case "0":
                 return;
         }
-
+        SaveDictToFile();
     }
 }
 
 void Learn()
 {
-    var exitWord = ":q";
-
-    Console.Write("Set rate: ");
-    var rate = Convert.ToDecimal(CustomRead());
-    Console.Write("Set limit: ");
-    var lim = Convert.ToInt32(CustomRead());
-    var learningWords = dict.Where(x => stat[x.Key].LearningRate <= rate).Select(x => x.Key).ToList();
-    learningWords = learningWords.GetRange(0, lim > learningWords.Count ? learningWords.Count : lim);
-    var wordsCount = learningWords.Count;
-    var rounds = 1;
-    var exit = false;
-    var rand = new Random();
-
-    Console.WriteLine("========LEARNING========");
-    while(wordsCount != 0)
+    try
     {
-        Console.WriteLine($"========ROUND #{rounds}========");
-        Console.WriteLine($"====WORDS TO LEARN {wordsCount}====");
+        var exitWord = ":q";
 
-        var marked = new bool[wordsCount];
-        var markedCnt = 0;
-        var goodAns = 0;
-        
-        var wordsToDel = new List<string>();
+        Console.Write("Set rate: ");
+        var rate = Convert.ToDecimal(CustomRead());
+        Console.Write("Set limit: ");
+        var lim = Convert.ToInt32(CustomRead());
+        var learningWords = dict.Where(x => stat[x.Key].LearningRate <= rate).Select(x => x.Key).ToList();
+        learningWords = SelectRandomNElems(learningWords, lim);
+        var wordsCount = learningWords.Count;
+        var rounds = 1;
+        var exit = false;
+        var rand = new Random();
 
-        while(markedCnt != wordsCount)
+        Console.WriteLine("========LEARNING========");
+        while (wordsCount != 0)
         {
-            var randNum = rand.Next(wordsCount);
+            Console.WriteLine($"========ROUND #{rounds}========");
+            Console.WriteLine($"====WORDS TO LEARN {wordsCount}====");
 
-            if (marked[randNum])
-                continue;
-            
-            marked[randNum] = true;
-            markedCnt++;
+            var marked = new bool[wordsCount];
+            var markedCnt = 0;
+            var goodAns = 0;
 
-            var randEl = learningWords.ElementAt(randNum);
-            Console.Write(randEl + " - ");
-            var ans = CustomRead();
-            if (ans == exitWord)
+            var wordsToDel = new List<string>();
+
+            while (markedCnt != wordsCount)
             {
-                exit = true;
+                var randNum = rand.Next(wordsCount);
+
+                if (marked[randNum])
+                    continue;
+
+                marked[randNum] = true;
+                markedCnt++;
+
+                var randEl = learningWords.ElementAt(randNum);
+                Console.Write($"[{markedCnt}/{wordsCount}]:\n" + randEl + " - ");
+                var ans = CustomRead();
+                if (ans == exitWord)
+                {
+                    exit = true;
+                    break;
+                }
+                if (!stat.ContainsKey(randEl))
+                    stat.Add(randEl, new Stat());
+
+                stat[randEl].Asked++;
+
+                if (ans == dict[randEl])
+                {
+                    stat[randEl].TrueAnswers++;
+                    Console.WriteLine($"Good! [{stat[randEl].TrueAnswers} of {stat[randEl].Asked}]");
+                    goodAns++;
+                    wordsToDel.Add(randEl);
+                }
+                else
+                {
+                    Console.WriteLine($"Wrong! [{stat[randEl].TrueAnswers} of {stat[randEl].Asked}]");
+                    Console.WriteLine($"{randEl} - {dict[randEl]}");
+                }
+            }
+            learningWords = learningWords.Where(x => !wordsToDel.Contains(x)).ToList();
+            Console.WriteLine($"=========={goodAns} of {wordsCount}===========");
+            wordsCount = learningWords.Count;
+
+            if (exit)
                 break;
-            }
-            if (!stat.ContainsKey(randEl))
-                stat.Add(randEl, new Stat());
-
-            stat[randEl].Asked++;
-
-            if (ans == dict[randEl])
-            {
-                Console.WriteLine("Good!");
-                stat[randEl].TrueAnswers++;
-                goodAns++;
-                wordsToDel.Add(randEl);
-            }
-            else
-            {
-                Console.WriteLine("Wrong!");
-                Console.WriteLine($"{randEl} - {dict[randEl]}");
-            }
         }
-        learningWords = learningWords.Where(x => !wordsToDel.Contains(x)).ToList();
-        Console.WriteLine($"=========={goodAns} of {wordsCount}===========");
-        wordsCount = learningWords.Count;
 
-        if (exit)
-            break;
+        Console.WriteLine("==========EXIT===========");
     }
-    
-    Console.WriteLine("==========EXIT===========");
+    catch (Exception e)
+    {
+        Console.WriteLine($"Cought some exeption {e}");
+        Console.WriteLine("==========EXIT===========");
+        return;
+    }
+}
+
+List<string> SelectRandomNElems(List<string> learningWords, int lim)
+{
+    var randRange = learningWords.Count;
+
+    if (lim >= randRange)
+        return learningWords;
+
+    var kek = new Random();
+    var visited = new HashSet<int>();
+    var resList = new List<string>();
+
+    var i = 0;
+    while (i < lim)
+    {
+        var currNum = kek.Next(randRange);
+        if (visited.Contains(currNum))
+            continue;
+
+        resList.Add(learningWords[currNum]);
+        visited.Add(currNum);
+        ++i;
+    }
+
+    return resList;
 }
 
 void UpdateStat()
@@ -171,16 +206,16 @@ void FindWord()
 {
     Console.WriteLine("Enter word to find:");
     var findWord = CustomRead();
-    
+
     if (dict.ContainsKey(findWord))
         Console.WriteLine($"{findWord} - {dict[findWord]}");
-    else 
+    else
         Console.WriteLine($"No such word \"{findWord}\" in dictionary!");
 }
 
 void AddWordToDict()
 {
-    while(true)
+    while (true)
     {
         Console.Write("Enter word on Eng: ");
         var engWord = CustomRead();
@@ -188,7 +223,7 @@ void AddWordToDict()
             break;
         Console.Write("Enter word on Rus: ");
         var rusWord = CustomRead();
-        
+
         if (dict.ContainsKey(engWord))
         {
             Console.WriteLine("Dictionary already contains this word!");
@@ -201,23 +236,23 @@ void AddWordToDict()
         stat.Add(engWord, new Stat());
         SaveDictToFile(false);
     }
-    
+
 }
 
 void DeleteWord()
 {
     Console.WriteLine("Enter word to delete:");
     var deleteWord = CustomRead();
-    
+
     if (dict.ContainsKey(deleteWord))
     {
         dict.Remove(deleteWord);
         Console.WriteLine($"\"{deleteWord}\" was deleted!");
     }
-    else 
+    else
         Console.WriteLine($"No such word \"{deleteWord}\" in dictionary!");
 }
-void SaveDictToFile(bool log=true)
+void SaveDictToFile(bool log = true)
 {
     try
     {
@@ -230,7 +265,7 @@ void SaveDictToFile(bool log=true)
         if (log)
             Console.WriteLine("Dictionary saved!");
     }
-    catch(Exception e)
+    catch (Exception e)
     {
         Console.WriteLine(e);
     }
@@ -239,8 +274,8 @@ void SaveDictToFile(bool log=true)
 void PrintDict()
 {
     Console.WriteLine("-------------------------------");
-    
-    foreach(var el in dict)
+
+    foreach (var el in dict)
     {
         if (!stat.ContainsKey(el.Key))
             stat.Add(el.Key, new Stat());
