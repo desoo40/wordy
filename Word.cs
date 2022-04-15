9ad81d65e14098163dc58cbf4a5b22fb3d4d02fd
Denis Sorokin <desoo@Deniss-MacBook-Pro.local>
2022-04-16 01:37:58 +0300

    word approach

--- Word.cs
+++ Word.cs
@@ -3,15 +3,27 @@ public class Word
     public string EngWord {get; set;}
     public string RusWord {get; set;}
 
-    public int timesAsked;
-    public int correctAnswers;
+    public int engToRusTimeAsked;
+    public int engToRusCorrectAnswers;
+    public int rusToEngTimeAsked;
+    public int rusToEngCorrectAnswers;
+
+    public decimal RusToEngLearningRate
+    {
+        get => rusToEngTimeAsked == 0 ? 0 : (decimal)rusToEngCorrectAnswers / rusToEngTimeAsked;
+    }
+
+    public decimal EngToRusLearningRate
+    {
+        get => engToRusTimeAsked == 0 ? 0 : (decimal)engToRusCorrectAnswers / engToRusTimeAsked;
+    }
 
     public Word(string en, string ru)
     {
         EngWord = en;
         RusWord = ru;
-        timesAsked = 0;
-        correctAnswers = 0;
+        engToRusTimeAsked = 0;
+        rusToEngTimeAsked = 0;
     }
 
     public override string ToString()
