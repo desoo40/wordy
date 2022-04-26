bbd413a99f2b048d7d31c6fdba51388692c50055
Denis Sorokin <desoo@Deniss-MacBook-Pro.local>
2022-04-27 01:19:48 +0300

    some changes

--- Program.cs
+++ Program.cs
@@ -1,25 +1,98 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Newtonsoft.Json;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
-var userName = Login();
-var userPath = $"{userName}.json";
-var genDict = new WordsDict(userPath);
+var usersBase = new Dictionary<string, string>();
 
-Menu();
-genDict.SaveToFile(userPath);
+var botClient = new TelegramBotClient(System.IO.File.ReadAllText("config.stgs"));
 
-string Login()
+using var cts = new CancellationTokenSource();
+
+// StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+var receiverOptions = new ReceiverOptions
 {
-    Console.WriteLine("Enter your login:");
-    return Utils.CustomRead();
+    AllowedUpdates = { } // receive all update types
+};
+botClient.StartReceiving(
+    HandleUpdateAsync,
+    HandleErrorAsync,
+    receiverOptions,
+    cancellationToken: cts.Token);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+
+// Send cancellation request to stop bot
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Type != UpdateType.Message)
+        return;
+    // Only process text messages
+    if (update.Message!.Type != MessageType.Text)
+        return;
+
+    var chatId = update.Message.Chat.Id;
+    var messageText = update.Message.Text;
+
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+    // Echo received message text
+    Message sentMessage = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text: "You said:\n" + messageText,
+        cancellationToken: cancellationToken);
 }
 
-void Menu()
+Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
 {
-    while (true)
+    var ErrorMessage = exception switch
     {
-        Console.WriteLine(
-@"Enter action:
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
+
+
+
+
+
+
+
+void ConsoleMode()
+{
+    // See https://aka.ms/new-console-template for more information
+    var userName = Login();
+    var userPath = $"{userName}.json";
+    var genDict = new WordsDict(userPath);
+
+    Menu();
+    genDict.SaveToFile(userPath);
+
+    string Login()
+    {
+        Console.WriteLine("Enter your login:");
+        return Utils.CustomRead();
+    }
+
+    void Menu()
+    {
+        while (true)
+        {
+            Console.WriteLine(
+    @"Enter action:
     1 - Add word
     2 - Print dict
     3 - Save to file
@@ -30,210 +103,211 @@ void Menu()
     8 - Learn words (Eng to Rus)
     0 - Exit");
 
-        var ans = Utils.CustomRead();
+            var ans = Utils.CustomRead();
 
-        switch (ans)
-        {
-            case "1":
-                AddWordToDict();
-                break;
-            case "2":
-                genDict.Print();
-                break;
-            case "3":
-                genDict.SaveToFile(userPath);
-                break;
-            case "4":
-                DeleteWord();
-                break;
-            case "5":
-                FindWord();
-                break;
-            case "6":
-                Learn(LangMode.Mixed);
-                break;
-            case "7":
-                Learn(LangMode.RusToEn);
-                break;
-            case "8":
-                Learn(LangMode.EnToRus);
-                break;
-            case "0":
-                return;
-        }
-
-        genDict.SaveToFile(userPath);
-    }
-}
-
-void DeleteWord()
-{
-    Console.WriteLine("Enter word to delete:");
-    var deleteWord = Utils.CustomRead();
-    var res = genDict.DeleteWord(deleteWord);
-
-    if (res)
-        Console.WriteLine($"\"{deleteWord}\" was deleted!");
-    else
-        Console.WriteLine($"No such word \"{deleteWord}\" in dictionary!");
-}
-
-void AddWordToDict()
-{
-    while (true)
-    {
-        Console.Write("Enter word on Eng: ");
-        var engWord = Utils.CustomRead();
-        if (engWord == ":q")
-            break;
-
-        Console.Write("Enter word on Rus: ");
-        var rusWord = Utils.CustomRead();
-        if (rusWord == ":q")
-            break;
-
-        if (genDict.Find(engWord) != null)
-        {
-            Console.WriteLine("Dictionary already contains this word!");
-            continue;
-        }
-
-        var res = genDict.Add(engWord, rusWord);
-
-        if (res)
-            Console.WriteLine("Added to dictionary!");
-        else
-            Console.WriteLine("Something go wrong!");
-
-        genDict.SaveToFile(userPath, false);
-    }
-}
-
-void FindWord()
-{
-    Console.WriteLine("Enter word to find:");
-    var input = Utils.CustomRead();
-    var word = genDict.Find(input);
-
-    if (word == null)
-        Console.WriteLine($"No such word \"{input}\" in dictionary!");
-    else
-        Console.WriteLine(word);
-}
-
-void Learn(LangMode mode)
-{
-    try
-    {
-        var exitWord = ":q";
-
-        Console.Write("Set rate: ");
-        var rate = Convert.ToDecimal(Utils.CustomRead());
-
-        Console.Write("Set limit: ");
-        var lim = Convert.ToInt32(Utils.CustomRead());
-
-        var learningWords = genDict.GetRandomNElemsUnderRate(lim, rate, mode);
-        var wordsCount = learningWords.Count;
-        var rounds = 1;
-        var exit = false;
-        var rand = new Random();
-
-        Console.WriteLine("========LEARNING========");
-        while (wordsCount != 0)
-        {
-            Console.WriteLine($"========ROUND #{rounds}========");
-            Console.WriteLine($"====WORDS TO LEARN {wordsCount}====");
-
-            var marked = new bool[wordsCount];
-            var markedCnt = 0;
-            var goodAns = 0;
-
-            var wordsToDel = new List<Word>();
-
-            while (markedCnt != wordsCount)
+            switch (ans)
             {
-                var randNum = rand.Next(wordsCount);
-
-                if (marked[randNum])
-                    continue;
-
-                marked[randNum] = true;
-                markedCnt++;
-
-                var tmpMode = LangMode.EnToRus;
-                if (mode == LangMode.Mixed && randNum % 2 == 0)
-                    tmpMode = LangMode.RusToEn;
-
-                var randEl = learningWords.ElementAt(randNum);
-
-                var askWord = randEl.EngWord;
-                var ansWord = randEl.RusWord;
-                var tmpAsked = randEl.engToRusAsked;
-                var tmpTrue = randEl.engToRusTrueAnswers;
-
-                if (tmpMode != LangMode.EnToRus)
-                {
-                    askWord = randEl.RusWord;
-                    ansWord = randEl.EngWord;
-                    tmpAsked = randEl.rusToEngAsked;
-                    tmpTrue = randEl.rusToEngTrueAnswers;
-                }
-
-                Console.Write($"[{markedCnt}/{wordsCount}]:\n" + askWord + " - ");
-
-                var ans = Utils.CustomRead();
-                if (ans == exitWord)
-                {
-                    exit = true;
+                case "1":
+                    AddWordToDict();
                     break;
-                }
-
-                tmpAsked++;
-
-                if (ans == ansWord)
-                {
-                    tmpTrue++;
-                    Console.WriteLine($"Good! [{tmpTrue} of {tmpAsked}]");
-                    goodAns++;
-                    wordsToDel.Add(randEl);
-                }
-                else
-                {
-                    Console.WriteLine($"Wrong! [{tmpTrue} of {tmpAsked}]");
-                    Console.WriteLine($"{askWord} - {ansWord}");
-                }
-
-                if (tmpMode == LangMode.EnToRus)
-                {
-                    randEl.EngWord = askWord;
-                    randEl.RusWord = ansWord;
-                    randEl.engToRusAsked = tmpAsked;
-                    randEl.engToRusTrueAnswers = tmpTrue;
-                }
-                else
-                {
-                    randEl.RusWord = askWord;
-                    randEl.EngWord = ansWord;
-                    randEl.rusToEngAsked = tmpAsked;
-                    randEl.rusToEngTrueAnswers = tmpTrue;
-                }
+                case "2":
+                    genDict.Print();
+                    break;
+                case "3":
+                    genDict.SaveToFile(userPath);
+                    break;
+                case "4":
+                    DeleteWord();
+                    break;
+                case "5":
+                    FindWord();
+                    break;
+                case "6":
+                    Learn(LangMode.Mixed);
+                    break;
+                case "7":
+                    Learn(LangMode.RusToEn);
+                    break;
+                case "8":
+                    Learn(LangMode.EnToRus);
+                    break;
+                case "0":
+                    return;
             }
 
-            learningWords = learningWords.Where(x => !wordsToDel.Contains(x)).ToList();
-            Console.WriteLine($"=========={goodAns} of {wordsCount}===========");
-            wordsCount = learningWords.Count;
-
-            if (exit)
-                break;
+            genDict.SaveToFile(userPath);
         }
-
-        Console.WriteLine("==========EXIT===========");
     }
-    catch (Exception e)
+
+    void DeleteWord()
     {
-        Console.WriteLine($"Cought some exeption {e}");
-        Console.WriteLine("==========EXIT===========");
-        return;
+        Console.WriteLine("Enter word to delete:");
+        var deleteWord = Utils.CustomRead();
+        var res = genDict.DeleteWord(deleteWord);
+
+        if (res)
+            Console.WriteLine($"\"{deleteWord}\" was deleted!");
+        else
+            Console.WriteLine($"No such word \"{deleteWord}\" in dictionary!");
+    }
+
+    void AddWordToDict()
+    {
+        while (true)
+        {
+            Console.Write("Enter word on Eng: ");
+            var engWord = Utils.CustomRead();
+            if (engWord == ":q")
+                break;
+
+            Console.Write("Enter word on Rus: ");
+            var rusWord = Utils.CustomRead();
+            if (rusWord == ":q")
+                break;
+
+            if (genDict.Find(engWord) != null)
+            {
+                Console.WriteLine("Dictionary already contains this word!");
+                continue;
+            }
+
+            var res = genDict.Add(engWord, rusWord);
+
+            if (res)
+                Console.WriteLine("Added to dictionary!");
+            else
+                Console.WriteLine("Something go wrong!");
+
+            genDict.SaveToFile(userPath, false);
+        }
+    }
+
+    void FindWord()
+    {
+        Console.WriteLine("Enter word to find:");
+        var input = Utils.CustomRead();
+        var word = genDict.Find(input);
+
+        if (word == null)
+            Console.WriteLine($"No such word \"{input}\" in dictionary!");
+        else
+            Console.WriteLine(word);
+    }
+
+    void Learn(LangMode mode)
+    {
+        try
+        {
+            var exitWord = ":q";
+
+            Console.Write("Set rate: ");
+            var rate = Convert.ToDecimal(Utils.CustomRead());
+
+            Console.Write("Set limit: ");
+            var lim = Convert.ToInt32(Utils.CustomRead());
+
+            var learningWords = genDict.GetRandomNElemsUnderRate(lim, rate, mode);
+            var wordsCount = learningWords.Count;
+            var rounds = 1;
+            var exit = false;
+            var rand = new Random();
+
+            Console.WriteLine("========LEARNING========");
+            while (wordsCount != 0)
+            {
+                Console.WriteLine($"========ROUND #{rounds}========");
+                Console.WriteLine($"====WORDS TO LEARN {wordsCount}====");
+
+                var marked = new bool[wordsCount];
+                var markedCnt = 0;
+                var goodAns = 0;
+
+                var wordsToDel = new List<Word>();
+
+                while (markedCnt != wordsCount)
+                {
+                    var randNum = rand.Next(wordsCount);
+
+                    if (marked[randNum])
+                        continue;
+
+                    marked[randNum] = true;
+                    markedCnt++;
+
+                    var tmpMode = LangMode.EnToRus;
+                    if (mode == LangMode.Mixed && randNum % 2 == 0)
+                        tmpMode = LangMode.RusToEn;
+
+                    var randEl = learningWords.ElementAt(randNum);
+
+                    var askWord = randEl.EngWord;
+                    var ansWord = randEl.RusWord;
+                    var tmpAsked = randEl.engToRusAsked;
+                    var tmpTrue = randEl.engToRusTrueAnswers;
+
+                    if (tmpMode != LangMode.EnToRus)
+                    {
+                        askWord = randEl.RusWord;
+                        ansWord = randEl.EngWord;
+                        tmpAsked = randEl.rusToEngAsked;
+                        tmpTrue = randEl.rusToEngTrueAnswers;
+                    }
+
+                    Console.Write($"[{markedCnt}/{wordsCount}]:\n" + askWord + " - ");
+
+                    var ans = Utils.CustomRead();
+                    if (ans == exitWord)
+                    {
+                        exit = true;
+                        break;
+                    }
+
+                    tmpAsked++;
+
+                    if (ans == ansWord)
+                    {
+                        tmpTrue++;
+                        Console.WriteLine($"Good! [{tmpTrue} of {tmpAsked}]");
+                        goodAns++;
+                        wordsToDel.Add(randEl);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Wrong! [{tmpTrue} of {tmpAsked}]");
+                        Console.WriteLine($"{askWord} - {ansWord}");
+                    }
+
+                    if (tmpMode == LangMode.EnToRus)
+                    {
+                        randEl.EngWord = askWord;
+                        randEl.RusWord = ansWord;
+                        randEl.engToRusAsked = tmpAsked;
+                        randEl.engToRusTrueAnswers = tmpTrue;
+                    }
+                    else
+                    {
+                        randEl.RusWord = askWord;
+                        randEl.EngWord = ansWord;
+                        randEl.rusToEngAsked = tmpAsked;
+                        randEl.rusToEngTrueAnswers = tmpTrue;
+                    }
+                }
+
+                learningWords = learningWords.Where(x => !wordsToDel.Contains(x)).ToList();
+                Console.WriteLine($"=========={goodAns} of {wordsCount}===========");
+                wordsCount = learningWords.Count;
+
+                if (exit)
+                    break;
+            }
+
+            Console.WriteLine("==========EXIT===========");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Cought some exeption {e}");
+            Console.WriteLine("==========EXIT===========");
+            return;
+        }
     }
 }
--- Utils.cs
+++ Utils.cs
@@ -1,7 +1,35 @@
+using Newtonsoft.Json;
+
 public static class Utils
 {
+    static string usersPath = "database\\users.json";
     public static string CustomRead()
     {
         return Console.ReadLine().Trim();
+    }
+
+    public static void GetUsersBase(Dictionary<string, string> users)
+    {
+        try
+        {
+            var json = JsonConvert.SerializeObject(users);
+            File.WriteAllText(usersPath, json);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    public static Dictionary<string, string> UpdateUsersBase()
+    {
+        if (File.Exists(usersPath))
+        {
+            var jsonText = File.ReadAllText(usersPath);
+            var jsonRes = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonText);
+
+            return jsonRes;
+        }
+        return null;
     }
 }
